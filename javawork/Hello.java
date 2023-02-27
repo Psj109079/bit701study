e4a899ea888ca0767d7db6f5ac31164743631330
Psj <ckckckemfdjdhk@gmail.com>
2023-02-27 11:35:27 +0900

    first commit

---
+++ javawork/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Java Class를 메모장으로 만들었어요!");
+	}
+}
---
+++ javawork/Java701project/src/study/day0227/Ex1_DataType.java
@@ -0,0 +1,9 @@
+package study.day0227;
+
+public class Ex1_DataType {
+
+	public static void main(String[] args) {
+		System.out.println("자바 프로젝트 생성");
+	}
+
+}
